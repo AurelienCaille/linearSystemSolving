0e02c1a83b969ab1e5a061bf925f0370017495d6
paulinesthln <pauline@odala.com>
2016-11-07 16:09:14 +0100

    Adding repr methode

--- Matrix.py
+++ Matrix.py
@@ -5,7 +5,9 @@ class Matrix:
         """create an empty matrix
         """
         self.matrix = matrix
+    
     def multiply(self, matrix):
+        
         """definit le protuit matriciel
         """
         result = []
@@ -17,11 +19,26 @@ class Matrix:
                     somme += self.matrix[i][k]*matrix.matrix[k][j]
                 line.append(somme)
             result.append(line)
-        return result
+        return Matrix(result)
+    
+    def __repr__(self):
+        result = ""
+        for i in range(len(self.matrix)):
+            result = result + "( "
+            for j in range(len(self.matrix)): 
+                 result = result + str(self.matrix[i][j]) + " "
+            result = result + ")\n"
+        result = result +"-------------"
+        return result.strip()
                 
 if __name__ == "__main__":
-    A = [[1, 2],[3, 4]]
-    B = [[5, 6],[7, 8]]
+    A = [[1, 2, 3],
+         [3, 4, 5],
+         [1, 2, 3]]
+    
+    B = [[5, 6, 10],
+         [7, 8, 45],
+         [1, 2, 3]]
     A = Matrix(A)
     B = Matrix(B)
     print (A.multiply(B))

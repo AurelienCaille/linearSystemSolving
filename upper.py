43bc61145570db3deeccc27e306184a3c646c0d0
Lievre Antoine <alievre@laposte.net>
2016-12-05 13:57:16 +0100

    deleting lower function

--- upper.py
+++ upper.py
@@ -1,9 +1,11 @@
 import Matrix
 from Rational import Rational as R
 def upper(matrix):
-
+    """
+    return the upper matrix of a matrix
+    """
     nb_line = len(matrix.matrix)
-    An = A
+    An = matrix
     for n in range (nb_line-1):
         
         E = []
@@ -27,46 +29,20 @@ def upper(matrix):
                                     
         E = Matrix.Matrix(E)
         
-        An = E.multiply(An)
+        An = E * An
     return An
 
 
 def delta(i, j):
+    """
+    return 1 if i == j
+    return 0 if i != j
+    """
     if i == j:
         return 1
     else:
         return 0
-def lower(matrix):
-#pas fini
-    nb_line = len(matrix.matrix)
-    En = Matrix.Matrix([[delta(i, j) for i in range(nb_line)]
-                                    for j in range(nb_line)])
-    for n in range (nb_line - 1):
         
-        E = []
-        for i in range(nb_line):
-            E.append([])
-            
-        for x in range (nb_line):
-            for k in range (nb_line):
-                if x == k:
-                    E[k].append(1)
-                    continue
-                else :
-                    if n == x:
-                        if x>k:
-                            E[k].append( An[k][x] / An[n][n])
-                            
-                            continue
-                        
-                E[k].append(0)
-                
-                                    
-        E = Matrix.Matrix(E)
-        En = E.multiply(En)
-        print(E)
-        print(En)
-    return En
 
 if __name__ == "__main__":
     A = Matrix.Matrix([[R(2), R(3), R(3), R(1)],

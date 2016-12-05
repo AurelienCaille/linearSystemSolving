4a2cc2e8c99aea4cef169accf9270c593fcd624b
AurelienCaille <aur-lien@hotmail.fr>
2016-12-05 15:00:14 +0100

    Add some comments

--- lower.py
+++ lower.py
@@ -1,27 +1,30 @@
+import Matrix
+from upper import delta
+
 def lower(matrix):
 #pas fini
     nb_line = len(matrix.matrix)
     En = Matrix.Matrix([[delta(i, j) for i in range(nb_line)]
                                     for j in range(nb_line)])
-    for n in range (nb_line - 1):
+    for n in range (nb_line - 1): #E_{n-1} ... E_{1}
         
+        #Create empty matrix
         E = []
         for i in range(nb_line):
             E.append([])
-            
-        for x in range (nb_line):
-            for k in range (nb_line):
-                if x == k:
-                    E[k].append(1)
+        
+        #Create E_{n}^{-1} matrix
+        for column in range (nb_line):
+            for line in range (nb_line):
+                if column == line: #in the diagonal
+                    E[line].append(1)
                     continue
                 else :
-                    if n == x:
-                        if x>k:
-                            E[k].append( An[k][x] / An[n][n])
-                            
-                            continue
+                    if n == column and column>line:
+                        E[line].append( An[line][column] / An[n][n])
+                        continue
                         
-                E[k].append(0)
+                E[line].append(0)
                 
                                     
         E = Matrix.Matrix(E)
@@ -29,3 +32,8 @@ def lower(matrix):
         print(E)
         print(En)
     return En
+
+
+if __name__ == "__main__" :
+    pass
+

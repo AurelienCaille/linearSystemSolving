629453e48c0784239c5535fdcc752e9444f8356a
Caille Aurelien <aur-lien@hotmail.fr>
2017-01-14 19:32:39 +0100

    Init interface

---
+++ LinearSystemInterface.py
@@ -0,0 +1,12 @@
+import sys
+
+from PyQt5.QtWidgets import QMainWindow, QFrame, QDesktopWidget, QApplication
+from PyQt5.QtGui import QPainter, QColor
+
+
+class LinearSystemInterface(QMainWindow):
+
+    def __init__(self):
+
+        super().__init__()
+        pass

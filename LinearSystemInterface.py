5078522cbbed2f4006b1bcbacee4999c21a2b221
Aurelien <Aurelien Caille>
2017-01-15 18:08:30 +0100

    begin init method

--- LinearSystemInterface.py
+++ LinearSystemInterface.py
@@ -4,9 +4,53 @@ from PyQt5.QtWidgets import QMainWindow, QFrame, QDesktopWidget, QApplication
 from PyQt5.QtGui import QPainter, QColor
 
 
+class MatrixInputWidget(QWidget):
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        plusButton = QPushButton("+")
+        lessButton = QPushButton("-")
+
+        
+
+
+        
+
+    
+
+
 class LinearSystemInterface(QMainWindow):
 
     def __init__(self):
 
         super().__init__()
-        pass
+
+        self.initUI()
+
+    def initUI(self):
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle("Linear System Solving Interface")
+
+        menubar = self.menuBar()
+        lu_decomposition = menubar.addMenu('LU')
+        jacobi = menubar.addMenu('Jacobi')
+        
+        
+        self.statusBar().showMessage('Ready')
+        self.show()
+
+
+
+if __name__ == "__main__":
+
+    
+    app = QApplication(sys.argv)
+    lsi = LinearSystemInterface()
+    sys.exit(app.exec_())

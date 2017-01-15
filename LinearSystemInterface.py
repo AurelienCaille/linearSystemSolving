52b0704d7343e923f7e722a49ed2169f92be05fb
Aurelien <Aurelien Caille>
2017-01-15 17:37:00 +0100

    add more subs widgets

--- LinearSystemInterface.py
+++ LinearSystemInterface.py
@@ -1,10 +1,11 @@
 import sys
 
-from PyQt5.QtWidgets import QMainWindow, QFrame, QDesktopWidget, QApplication
+from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QPainter, QColor
 
 
 class MatrixInputWidget(QWidget):
+    """Widget to enter the value of a square matrix and modify its size"""
 
     def __init__(self):
 
@@ -14,18 +15,56 @@ class MatrixInputWidget(QWidget):
 
     def initUI(self):
 
+        #Button to modify the size
         plusButton = QPushButton("+")
         lessButton = QPushButton("-")
 
-        
+        #Grid for layout
+        grid = QGridLayout()
+        self.setLayout(grid)
+
+        #Add widget to grid
+        grid.addWidget(plusButton, 0, 0)
+        grid.addWidget(lessButton, 2, 2)
+
+
+        self.show()
+
+class VectorInputWidget(QWidget)
+    """Widget to enter a column vector"""
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        pass       
 
 
         
+class SystemInputWidget(QWidget)
+    """Widget to enter a linear system"""
 
+    def __init__(self):
+
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        pass
+        
     
 
 
 class LinearSystemInterface(QMainWindow):
+    """Interface to enter a linearSystem, print LU decomposition and solve it with LU or Jacobi method"""
 
     def __init__(self):
 
@@ -41,6 +80,12 @@ class LinearSystemInterface(QMainWindow):
         menubar = self.menuBar()
         lu_decomposition = menubar.addMenu('LU')
         jacobi = menubar.addMenu('Jacobi')
+
+        miw = MatrixInputWidget()
+
+        self.setCentralWidget(miw)
+
+        
         
         
         self.statusBar().showMessage('Ready')
